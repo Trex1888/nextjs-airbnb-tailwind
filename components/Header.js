5bc4bea83d2c1afd8c485d3586950b7ee9c6ba69
Trex1888 <harpua123@gmail.com>
2021-08-15 04:58:25 -0500

    5th commit

--- components/Header.js
+++ components/Header.js
@@ -76,7 +76,9 @@ function Header({ placeholder }) {
       </div>
 
       <div className="flex items-center space-x-4 justify-end text-gray-500">
-        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <p className="hidden md:inline cursor-pointer hover:animate-bounce">
+          Become a host
+        </p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
 
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key: `${process.env.KEY}`,
+  },
 };
