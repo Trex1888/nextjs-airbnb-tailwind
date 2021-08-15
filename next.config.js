762096ef5a3f34131763ce3b2cdc8c1f26b55434
Trex1888 <harpua123@gmail.com>
2021-08-15 05:07:20 -0500

    6th commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     domains: ["links.papareact.com"],
   },
   env: {
-    mapbox_key: `${process.env.KEY}`,
+    mapbox_key:
+      "pk.eyJ1IjoibGxhbWE5NCIsImEiOiJja3NibWVwMDMwODhoMnRvMnB0ZGFueHVwIn0.9Wrxo4FHhac3UzLJa9tTtQ",
   },
 };
